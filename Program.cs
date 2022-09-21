51b6efa13e3b817af7e7c4a5dde4db13d40170ff
«Ekaterina <ezhova.kate@mail.ru>
2022-09-21 16:49:00 +0300

    Домашка 9

--- Program.cs
+++ Program.cs
@@ -28,6 +28,7 @@ M = 4; N = 8. -> ""4, 6, 7, 8"" */
 void NaturalNumbers()
 {
     Console.Clear();
+    Console.WriteLine("Задача 64:Напишите программу, которая выведет все натуральные числа в промежутке от M до N.");
     int firstNumber = new Random().Next(1, 5);
     Console.WriteLine("Полученное рандомно первое число: " + firstNumber);
     int secondNumber = new Random().Next(5, 9);
@@ -48,6 +49,7 @@ M = 4; N = 8. -> 30 */
 void SumNaturalNumbers()
 {
     Console.Clear();
+    Console.WriteLine("Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.");
     Console.Write("Введите первое число: ");
     int firstNumber = Convert.ToInt32(Console.ReadLine());
     Console.Write("Введите второе число: ");
@@ -68,37 +70,38 @@ void SumNaturalNumbers()
 Даны два неотрицательных числа m и n.
 m = 3, n = 2 -> A(m,n) = 29
  */
-void Akkerman(){
-Console.Clear();
-Console.WriteLine("Напишите программу вычисления функции Аккермана с помощью рекурсии.");
-Console.WriteLine();
-Console.Write("Введите первое число: ");
-int firstNumber = int.Parse(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int secondNumber = int.Parse(Console.ReadLine());
-Console.WriteLine();
-if (firstNumber < 0 || secondNumber < 0)
+void Akkerman()
 {
-    Console.WriteLine("Пожалуйста, введите положительные числа.");
-    return;
-}
-int result = AckermannFunction(firstNumber, secondNumber);
-Console.WriteLine("Полученный ответ по формуле Аккермана: " +result);
+    Console.Clear();
+    Console.WriteLine("Напишите программу вычисления функции Аккермана с помощью рекурсии.");
+    Console.WriteLine();
+    Console.Write("Введите первое число: ");
+    int firstNumber = int.Parse(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    int secondNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (firstNumber < 0 || secondNumber < 0)
+    {
+        Console.WriteLine("Пожалуйста, введите положительные числа.");
+        return;
+    }
+    int result = AckermannFunction(firstNumber, secondNumber);
+    Console.WriteLine("Полученный ответ по формуле Аккермана: " + result);
 
-int AckermannFunction(int firstNumber, int secondNumber)
-{
-    if (firstNumber == 0)
+    int AckermannFunction(int firstNumber, int secondNumber)
     {
-        return secondNumber + 1;
+        if (firstNumber == 0)
+        {
+            return secondNumber + 1;
+        }
+        else if (firstNumber > 0 && secondNumber == 0)
+        {
+            return AckermannFunction(firstNumber - 1, 1);
+        }
+        else if (firstNumber > 0 && secondNumber > 0)
+        {
+            return AckermannFunction(firstNumber - 1, AckermannFunction(firstNumber, secondNumber - 1));
+        }
+        return AckermannFunction(firstNumber, secondNumber);
     }
-    else if (firstNumber > 0 && secondNumber == 0)
-    {
-        return AckermannFunction(firstNumber - 1, 1);
-    }
-    else if (firstNumber > 0 && secondNumber > 0)
-    {
-        return AckermannFunction(firstNumber - 1, AckermannFunction(firstNumber, secondNumber - 1));
-    }
-    return AckermannFunction(firstNumber, secondNumber);
-}
 }
